5c7b1f9a4486997bd8049776c8cf6424b4e315b4
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-08 15:40:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -687,7 +687,7 @@ async def advantage_spell_chok(msg):
         return
     user = msg.from_user.id if msg.from_user else 0
     imdb_s = await get_poster(query, bulk=True)
-    movielist = [movie.get📁('title')🎥 for movie in imdb_s]
+    movielist = [📁movie.get('title') for movie in imdb_s🎥]
     splitted = query.split()
     if len(splitted) > 10:
         k = await msg.reply("Are you telling the story of some movie??")
